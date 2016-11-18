df6cd2c1d78df7863cd07b68a2f4da38985ab363
Ryan Borgeson <ryanborgeson@gmail.com>
2016-11-17 22:26:21 -0500

    Updated to include watchdog timer.

--- SecuritySystem/Protocols/I2C.c
+++ SecuritySystem/Protocols/I2C.c
@@ -9,11 +9,16 @@
 /* I2C Master Configuration Parameter */
 const eUSCI_I2C_MasterConfig i2cConfig =
 {
-	EUSCI_B_I2C_CLOCKSOURCE_SMCLK, 		// SMCLK Clock Source
-	48000000, 							// SMCLK = 3MHz
-	EUSCI_B_I2C_SET_DATA_RATE_400KBPS,  // Desired I2C Clock of 400khz
-	0,									// No byte counter threshold
-	EUSCI_B_I2C_NO_AUTO_STOP			// No Autostop
+	// SMCLK Clock Source
+	EUSCI_B_I2C_CLOCKSOURCE_SMCLK,
+	// SMCLK = 48MHz
+	48000000,
+	// Desired I2C Clock of 400khz
+	EUSCI_B_I2C_SET_DATA_RATE_400KBPS,
+	// No byte counter threshold
+	0,
+	// No Autostop
+	EUSCI_B_I2C_NO_AUTO_STOP
 };
 
 
